dc063d9268450b8d65127400ef4b7f633ca3080a
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-04-01 02:07:03 +0330

    create a read json helper function to keep the development at ease

--- internal/helper/json.go
+++ internal/helper/json.go
@@ -2,6 +2,9 @@ package helper
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -23,5 +26,32 @@ func WriteJSON(w http.ResponseWriter, status int, data Envelope, headers http.He
 	w.WriteHeader(status)
 	w.Write(js)
 
+	return nil
+}
+
+func ReadJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	if err := json.NewDecoder(r.Body).Decode(dst); err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON at character %d)", unmarshalTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		default:
+			return err
+		}
+	}
 	return nil
 }

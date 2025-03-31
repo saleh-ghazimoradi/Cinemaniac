d227cf462fc291a3f8676e5cc2962a50d071a523
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-03-31 23:26:53 +0330

    fix the issue of the healthcheck

--- internal/gateway/handlers/health.go
+++ internal/gateway/handlers/health.go
@@ -11,13 +11,15 @@ type HealthHandler struct {
 }
 
 func (h *HealthHandler) HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":      "available",
-		"environment": config.AppConfig.Server.Env,
-		"version":     config.AppConfig.Server.Version,
+	env := helper.Envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": config.AppConfig.Server.Env,
+			"version":     config.AppConfig.Server.Version,
+		},
 	}
 
-	if err := helper.WriteJSON(w, http.StatusOK, data, nil); err != nil {
+	if err := helper.WriteJSON(w, http.StatusOK, env, nil); err != nil {
 		slg.Logger.Error(err.Error())
 		http.Error(w, "The server encountered an internal error", http.StatusInternalServerError)
 	}

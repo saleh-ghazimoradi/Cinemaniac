e1d71825465f5085883b225a59e9d1abb7580b0a
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-03-31 22:48:21 +0330

    add write json helper function to keep the development at ease

---
+++ internal/helper/json.go
@@ -0,0 +1,25 @@
+package helper
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for k, v := range headers {
+		w.Header()[k] = v
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
